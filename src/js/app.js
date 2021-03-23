2541e9dee8ad5617f217bc3d205eac18a316b08f
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-03-22 20:34:48 -0400

    added some images and messed around with html and css.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-let donutCount = 0;
+let gameDonut = new Donut();
 
 load();
 
@@ -6,15 +6,79 @@ function load() {
     addEventListeners();
 }
 
-function addEventListener() {
-    document.getElementById("mainButton").addEventListener("click", buttonClick);
-    document.getElementById("MainButton").addEventListener("click", buttonClick);
-    document.getElementById("MainButton").addEventListener("click", buttonClick);
+function addEventListeners() {
+    document.getElementById("Main-Click__Button").addEventListener("click", mainButtonClick);
+    document.getElementById("AutoClickerBuy-Click__Button").addEventListener("click", buyAutoClicker);
+    document.getElementById("MultiplierBuy-Click__Button").addEventListener("click", buyClickMultipler);
+    document.getElementById("Restart-Click__Button").addEventListener("click", restartButtonClick);
 
-    document.getElementById("Donut_Count").innerHTML = donutCount;
+    document.getElementById("Donut-Count").innerHTML = "Donut Count: " + gameDonut.getDonutCount();
+    document.getElementById("Click-Value").innerHTML = "Click Value: " + gameDonut.getCurrentClickValue();
+    document.getElementById("AutoClicker-Cost").innerHTML = "Cost: " + gameDonut.getAutoClickerCost();
+    document.getElementById("Multiplier-Cost").innerHTML = "Cost: " + gameDonut.getClickMultiplierCost();
 }
 
+// Click
 function mainButtonClick() {
-    document.getElementById("Donut_Count").innerHTML = ++donutCount;
+    gameDonut.addDonutCount(gameDonut.getCurrentClickValue());
+
+    checkDonutCount();
 }
 
+// Click
+function buyClickMultipler() {
+    gameDonut.addMultiplierCount();
+    document.getElementById("Multiplier-Count").innerHTML = "Owned Multiplier Count: " + gameDonut.getClickMultiplierCount();
+    gameDonut.addDonutCount(-(gameDonut.getClickMultiplierCost()));
+    gameDonut.updateClickMultiplierCost();
+    gameDonut.updateCurrentClickValue();
+
+    checkDonutCount();
+}
+
+// Click
+function buyAutoClicker() {
+    StartAutoClick();
+
+    gameDonut.addAutoClicker();
+    document.getElementById("AutoClicker-Count").innerHTML =  "Owned AutoClicker Count: " + gameDonut.getAutoClickerCount();
+    gameDonut.addDonutCount(-(gameDonut.getAutoClickerCost()));
+    gameDonut.updateAutoClickerCost();
+
+    checkDonutCount();
+}
+
+function checkDonutCount() {
+    if (gameDonut.getDonutCount() >= gameDonut.getAutoClickerCost()) {
+        document.getElementById("AutoClickerBuy-Click__Button").disabled = false;
+    } 
+    else {
+        document.getElementById("AutoClickerBuy-Click__Button").disabled = true;
+    }
+
+    if (gameDonut.getDonutCount() >= gameDonut.getClickMultiplierCost()) {
+        document.getElementById("MultiplierBuy-Click__Button").disabled = false;
+    } 
+    else {
+        document.getElementById("MultiplierBuy-Click__Button").disabled = true;
+    }
+
+    updateHtmlElements();
+}
+
+function updateHtmlElements() {
+    document.getElementById("Donut-Count").innerHTML = "Donut Count: " + gameDonut.getDonutCount().toFixed(2);
+    document.getElementById("Click-Value").innerHTML = "Click Value: " + gameDonut.getCurrentClickValue().toFixed(2);
+    document.getElementById("AutoClicker-Cost").innerHTML = "Cost: " + gameDonut.getAutoClickerCost().toFixed(2);
+    document.getElementById("Multiplier-Cost").innerHTML = "Cost: " + gameDonut.getClickMultiplierCost().toFixed(2);
+}
+
+function StartAutoClick() {
+    window.setInterval(() => {
+        mainButtonClick();
+    }, 1000);
+}
+
+function restartButtonClick() {
+     window.location.reload();
+}
